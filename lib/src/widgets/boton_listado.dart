0023f99cc9c1992a95a85ffa4b189c13f88c0d69
DannyStrelok <hmartindaniel@gmail.com>
2020-12-25 18:47:34 +0100

    Incluidas animaciones al hacer scroll en el listado.

--- lib/src/widgets/boton_listado.dart
+++ lib/src/widgets/boton_listado.dart
@@ -1,15 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BotonListado extends StatelessWidget {
+  final IconData icon;
+  @required final String texto;
+  final Color color1;
+  final Color color2;
+  @required final Function onPress;
+
+  BotonListado(
+      {this.icon = FontAwesomeIcons.circle,
+      @required this.texto,
+      this.color1 = const Color(0xff6989f5),
+      this.color2 = const Color(0xff906ef5),
+      @required this.onPress});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: _Background(),
+    return GestureDetector(
+      onTap: this.onPress,
+      child: Stack(children: [
+        _Background(this.icon, this.color1, this.color2),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 140,
+              width: 40,
+            ),
+            FaIcon(
+              this.icon,
+              color: Colors.white,
+              size: 40,
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Expanded(
+                child: Text(
+              this.texto,
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            )),
+            FaIcon(
+              FontAwesomeIcons.chevronRight,
+              color: Colors.white,
+            ),
+            SizedBox(
+              width: 40,
+            ),
+          ],
+        )
+      ]),
     );
   }
 }
 
 class _Background extends StatelessWidget {
+
+  final IconData icon;
+  final Color color1;
+  final Color color2;
+
+
+  _Background(this.icon, this.color1, this.color2);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -26,7 +80,22 @@ class _Background extends StatelessWidget {
           ],
           borderRadius: BorderRadius.circular(15),
           gradient: LinearGradient(
-              colors: <Color>[Color(0xff6989f5), Color(0xff906ef5)])),
+              colors: <Color>[this.color1, this.color2])),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: Stack(
+          children: [
+            Positioned(
+                top: -20,
+                right: -20,
+                child: FaIcon(
+                  this.icon,
+                  size: 150,
+                  color: Colors.white.withOpacity(0.2),
+                ))
+          ],
+        ),
+      ),
     );
   }
 }
